22bb169d45d0d2f90b5154cba3365b835af4b075
Ayen-Athiak <ayenathiak@gmail.com>
2024-06-23 17:22:20 +0200

    simple files with arithmetic operation

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,8 @@
+# this a simple python code on how arithmetic operators 0perate on operands
+
+principal = 1000
+rate = 0.05
+time = 3
+interest = principal * rate * time
+print( "The simple interest is:",interest)
+

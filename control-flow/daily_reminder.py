8f64523f029e552cffc37bf9efc873dd9ca8ac85
Ayen-Athiak <ayenathiak@gmail.com>
2024-07-07 22:55:43 +0200

    looping still

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,16 +1,16 @@
-task = input("do you have task this week")
-priority = input("task’s priority (high, medium, low)")
-time_bound = input(" task is time-bound (yes or no)")
-match priority:
+Task = input("Enter your task:")
+Priority = input("Priority (high/medium/low):")
+Time_Bound = input("Is it time-bound? (yes/no):")
+match Priority:
     case"high":
         print("you really need to squeeze time for you work")
     case "medium":
         print("just work according this week")
     case "low":
         print("you still have time")
-    case _:print("just be patient")
 
 
-if time_bound.lower()== "yes":
+
+if Time_Bound.lower()== "yes":
     print("finish your work befor you sleep today")
 else:print("no need to rush")

6450b0590372eb0b923ba04b3ee6fafe2d5efc07
Ayen-Athiak <ayenathiak@gmail.com>
2024-07-11 15:45:58 +0200

    module with  functions

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,19 @@
+def perform_operation(num1,num2,operation):
+    #arithmetic_operations.py
+
+    num1 =float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number:"))
+    operation = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
+    if operation == "add":
+        return num1 + num2
+    elif operation == "subtract":
+        return num1 - num2
+    elif operation == "multiply":
+        return num1 * num2
+    elif operation == "divide":
+         if num2 == 0:
+            print("this number cannot be divide")
+    else:
+        return num2 / num2
+
+
---
+++ fns_and_dsa/main.py
@@ -0,0 +1,5 @@
+from arithmetic_operations import perform_operation
+
+result = perform_operation("num1","num2","operation")
+print(result)
+

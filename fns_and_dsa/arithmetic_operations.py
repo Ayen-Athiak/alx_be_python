94f6c54369b40d5203cd48e507a27889d17d402c
Ayen-Athiak <ayenathiak@gmail.com>
2024-07-12 05:56:22 +0200

    operation updated

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,25 +1,24 @@
-def perform_operation(num1,num2,operation):
+def perform_operation(num1, num2, operation):
     print("Arithmetic Operations")
-    num1 = float(input("Enter the first number: "))
-    num2 = float(input("Enter the second number: "))
-    operation = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
-    if operation =="add":
+    if operation == "add":
         return num1 + num2
-    elif operation == "subtract" :
+    elif operation == "subtract":
         return num1 - num2
-    elif operation =="multiply":
+    elif operation == "multiply":
         return num1 * num2
-    elif operation=="divide" and num2 == 0:
-        return "you number is indivisible"
-    else:return num1 /num2
+    elif operation == "divide":
+        if num2 == 0:
+            return "Your number is indivisible"
+        else:
+            return num1 / num2
+    else:
+        return "Invalid operation"
+
+# Example usage
 
 
+results = perform_operation(3, 0, "divide")
 
-
-
-results = perform_operation('num1','num2','operation')
-
-
-print(f"results:{results}")
+print(f"Results: {results}")
 
 

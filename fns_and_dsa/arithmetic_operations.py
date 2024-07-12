ebf33caddc3bf23d3e2cd8396f35f0a1bd1d06a3
Ayen-Athiak <ayenathiak@gmail.com>
2024-07-12 05:06:30 +0200

    list handling updated

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,4 +1,4 @@
-def perform_operation(num1,num2,operation):
+def perform_operation(Num1, Num2,Operation):
     print("Arithmetic Operations")
     num1 = float(input("Enter the first number: "))
     num2 = float(input("Enter the second number: "))
@@ -17,7 +17,7 @@ def perform_operation(num1,num2,operation):
 
 
 
-results = perform_operation("num1","num2","operation")
+results = perform_operation('num1','num2','operation')
 
 
 print(f"results:{results}")

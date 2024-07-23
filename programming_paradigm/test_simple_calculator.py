36630dd8b0a9902e57ca97c4f0b0295f16123cac
Ayen-Athiak <ayenathiak@gmail.com>
2024-07-24 01:08:05 +0200

    errors updated

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -3,22 +3,19 @@ from simple_calculator import SimpleCalculator
 
 class test_SimpleCalculator(unittest.TestCase ):
 
-    def test_add(self):
-        self.add = SimpleCalculator()
-        self.assertEqual(self.add .add( 4,7),11)
+    def test_addition(self):
+        self.calc = SimpleCalculator()
+        self.assertEqual(self.calc .add( 4,7),11)
 
-    def test_substract(self):
-        self.substract = SimpleCalculator()
-        self.assertEqual(self.substract.subtract(5,9),-4)
+    def test_substraction(self):
+        self.calc = SimpleCalculator()
+        self.assertEqual(self.calc.subtract(5,9),-4)
 
-    def test_multiply(self):
+    def test_multiplication(self):
 
-        self.multiply = SimpleCalculator()
-        self.assertEqual(self.multiply.multiply(20,3),60)
-    def test_divide(self):
-        self.divide =SimpleCalculator()
-        self.assertEqual(self.divide.divide(6,3),2)
+        self.calc = SimpleCalculator()
+        self.assertEqual(self.calc.multiply(20,3),60)
+    def test_division(self):
+        self.calc =SimpleCalculator()
+        self.assertEqual(self.calc.divide(6,3),2)
 
-    def test_Zerodivion(self):
-        self.Zerodivion =SimpleCalculator()
-        self.assertRaises()
